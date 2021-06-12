640842aeff7a34daa5bbbece36451a1cd36c5eeb
Vaisakh K P <vaisakh.kp@asista.com>
2021-06-12 17:48:20 +0530

    Initial Commit

---
+++ fitness-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fitness-app
+//
+//  Created by Asista Mac Mini on 12/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
